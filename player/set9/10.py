1cc3af8ec1ba48b0bc675c0cbaee11cadb902c90
gagangaur <gagan1999gaur@gmail.com>
2019-04-17 09:31:16 +0530

    update 1.9.0

---
+++ player/set9/10.py
@@ -0,0 +1,7 @@
+def fac(n):
+    if n==1:
+        return 1
+    return n*fac(n-1)
+n,r = map(int,input().split())
+print(fac(n)//(fac(n-r)*fac(r)))
+#abc
---
+++ player/set9/2.py
@@ -0,0 +1,8 @@
+n = int(input())
+l = list(map(int,input().split()))
+arr = l[0]
+
+for i in range(1,n):
+    arr &= l[i]
+print(arr)
+#abc
