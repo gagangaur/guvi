13efa42a5152be80476cbc2015b1cd343fb1a787
gagangaur <gagan1999gaur@gmail.com>
2019-04-17 09:24:35 +0530

    update 1.7.0

---
+++ player/set7/2.py
@@ -0,0 +1,8 @@
+n = int(input())
+min = 1
+while min<n:
+    if (n//min)%2 != 0 and n%min==0:
+        break
+    min += 1
+print(min)
+#abc
---
+++ player/set7/4.py
@@ -0,0 +1,8 @@
+n,k = map(int,input().split())
+l = sorted(list(map(int,input().split())))
+
+for i in l:
+    if i >= k:
+        break
+    print(i,end=' ')
+#abc
