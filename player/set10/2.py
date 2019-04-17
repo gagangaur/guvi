ace1dc6c5e9034ec91a1dfadf7d40e2cbc98021b
gagangaur <gagan1999gaur@gmail.com>
2019-04-17 09:34:13 +0530

    update 1.10.0

---
+++ player/set10/2.py
@@ -0,0 +1,7 @@
+def bcd(n):
+    if n==1 or n==0:
+        return n
+    return n%10+2*(bcd(n//10))
+
+print(bcd(int(input())))
+#avc
---
+++ player/set10/7.py
@@ -0,0 +1,8 @@
+n,k = map(int,input().split())
+if n%2 == 0:
+    n+=1
+s = 0
+for i in range(n,k,2):
+    s+=i
+print(s)
+#abc
