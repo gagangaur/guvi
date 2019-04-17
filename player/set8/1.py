2c6cae1912dacdc632c3b41be47b512e98058fb0
gagangaur <gagan1999gaur@gmail.com>
2019-04-17 09:28:17 +0530

    update 1.8.0

---
+++ player/set8/1.py
@@ -0,0 +1,12 @@
+n = int(input())
+l = list(map(int,input().split()))
+
+previous = l[0]
+
+for i in l[1:]:
+    if previous+1 == i:
+        print(i, end=' ')
+    elif previous-1 == i:
+        print(previous, end=' ')
+    previous = i
+#abc
